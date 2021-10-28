fcf4ebed2c6f13ad300d4c82bc1dcdc173ae4bab
Bezymno134 <65502046+Bezymno134@users.noreply.github.com>
2021-10-28 16:31:51 +0300

    Add files via upload

---
+++ Zadanie4.py
@@ -0,0 +1,22 @@
+n1 = 4
+n2 = -3
+n3 = 1
+i=0
+
+if n1<0:
+    pass
+else:
+    i+=1
+
+if n2<0:
+    pass
+else:
+    i+=1
+
+if n3<0:
+    pass
+else:
+    i+=1
+
+print("Количество положительных чисел = ", i)
+
